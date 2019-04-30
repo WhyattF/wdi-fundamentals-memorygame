2f3a5bfe09a5642e252c5c2e35cbe308909c5e16
Whyatt Foster <WhyattFoster@Whyatts-MacBook-Pro.local>
2019-04-30 04:52:27 -0400

    committing functions phase of memory game

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,23 +1,28 @@
-// alert('Hello, friends.');
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
 
-const cards = ["queen", "queen", "king", "king"];
-const cardsInPlay = [];
+// var cardOne = cards[0];
+// cardsInPlay.push(cardOne);
+// console.log("User flipped " + cardOne);
 
-var cardOne = cards[0];
-var cardTwo = cards[2];
-
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay.length === 2) {
-	if(cardsInPlay[0] === cardsInPlay[1])
-	{
-		console.log("you've found  match!"); 
-	}else
-	{
-		console.log("Sorry, try again."); 
+// // var cardTwo = cards[2];
+// cardsInPlay.push(cardTwo);
+// console.log("User flipped " + cardTwo);
+	
+	var checkForMatch = function () {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+  			console.log("You found a match!");
+		} else {
+  			console.log("Sorry, try again.");
+		}
 	}
+
+	var flipCard = function (cardId) {
+		console.log("User flipped " + cards[cardId]);
+		cardsInPlay.push(cards[cardId]);
+		if (cardsInPlay.length === 2) {
+			checkForMatch();
+	}	
 }
+flipCard(0);
+flipCard(2);
